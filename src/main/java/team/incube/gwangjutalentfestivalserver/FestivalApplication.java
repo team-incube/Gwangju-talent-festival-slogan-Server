29543678835a727613455c31bb7154ceedda818a
enbraining <me@bricn.net>
2025-04-27 17:06:42 +0900

    :recycle: 사소한 수정사항

--- src/main/java/team/incube/gwangjutalentfestivalserver/FestivalApplication.java
+++ src/main/java/team/incube/gwangjutalentfestivalserver/FestivalApplication.java
@@ -2,12 +2,14 @@ package team.incube.gwangjutalentfestivalserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
-public class GwangjuTalentFestivalServerApplication {
+public class FestivalApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GwangjuTalentFestivalServerApplication.class, args);
+        SpringApplication.run(FestivalApplication.class, args);
     }
 
 }
