cc6ba886eae88e7cfc439070f8fdb9ac140d4f87
김승찬 <s24041@gsm.hs.kr>
2025-05-05 22:44:19 +0900

    :recycle: 패키지 구조 변경

--- src/main/java/team/incube/gwangjutalentfestivalserver/global/thirdparty/google/adapter/GoogleSheetsService.java
+++ src/main/java/team/incube/gwangjutalentfestivalserver/global/thirdparty/google/adapter/GoogleSheetsService.java
@@ -1,5 +1,4 @@
-package team.incube.gwangjutalentfestivalserver.domain.slogan.service;
-
+package team.incube.gwangjutalentfestivalserver.global.thirdparty.google.adapter;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -11,7 +10,6 @@ import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.api.services.sheets.v4.model.ValueRange;
-
 
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
