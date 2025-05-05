f5cba51c97481cde76bdff5e36ace1ef74980409
김승찬 <s24041@gsm.hs.kr>
2025-05-05 20:29:36 +0900

    :sparkles: 필요한 dto 정의

---
+++ src/main/java/team/incube/gwangjutalentfestivalserver/domain/slogan/dto/request/SloganRequest.java
@@ -0,0 +1,33 @@
+package team.incube.gwangjutalentfestivalserver.domain.slogan.dto.request;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SloganRequest {
+
+    @NotBlank
+    private String slogan;
+
+    @NotBlank
+    private String description;
+
+    @NotBlank
+    private String school;
+
+    @NotNull
+    private Integer grade;
+
+    @NotNull
+    private Integer classNum;
+
+    @NotBlank
+    private String phone;
+}
+
